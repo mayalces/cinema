d0cb424d081c650784971647171c0534822b45cb
Mayra Alejandra Céspedes <mayalces@gmail.com>
2016-01-08 15:04:29 +0000

    Creating model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :bookings
+  resources :foods
+  resources :movie_rooms
+  resources :movie_showtimes
+  resources :movies
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
